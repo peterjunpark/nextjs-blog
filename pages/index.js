affff087029b595252aeb95d199a402120e454f5
Peter Park <peterjpark@pm.me>
2023-08-29 17:06:49 -0400

    noice

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi. I'm Peter. You are not.</p>
+        <p>Hi. I'm Peter.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
